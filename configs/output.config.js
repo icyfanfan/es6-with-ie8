cb35977e85462263687b77cd7693c4cbaa13402b
hzhuangdi <kisscat1990114@163.com>
2017-03-01 14:59:03 +0800

    fix output配置判断条件写反了

--- configs/output.config.js
+++ configs/output.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = function(options){
-    var filename = options.production?'js/[name].js':'js/[name]_[hash].js'
+    var filename = options.production?'js/[name]_[hash].js':'js/[name].js'
     return {
         path: options.path,
         // 输出文件名，如果不使用[name]，且chunk插件中未制定filename，则只会输出插件name文件 
